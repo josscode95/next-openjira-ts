33747a62bb4f544af2e34002de548e6002ec21f0
Jose Pardo <josmar.pm19@gmail.com>
2022-10-04 10:15:33 -0500

    create context

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+import Head from "next/head";
+import { FC, ReactElement } from "react";
+import { Navbar, Sidebar } from "../ui";
+
+interface LayoutI {
+  title?:string;
+  children:ReactElement
+}
+
+export const Layout:FC<LayoutI> = ({ title = 'OpenJira', children }) => {
+  return (
+    <Box sx={{ flexFlow: 1 }}>
+      <Head>
+        <title>{ title }</title>
+      </Head>
+
+      <Navbar />
+      <Sidebar />
+      
+      <Box sx={{ padding:'10px 20px' }}>
+        { children }
+      </Box>
+    </Box>
+  )
+}

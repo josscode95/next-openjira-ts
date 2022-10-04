24be2f2b3043cfec72354d8c70d25265e73d1dc6
Jose Pardo <josmar.pm19@gmail.com>
2022-10-04 12:29:50 -0500

    add functions in context

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,38 +1,79 @@
+import { ChangeEvent, useContext, useState } from 'react'
+
 import { Box, Button, TextField } from '@mui/material'
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 
-import React from 'react'
+import { EntriesContext } from '../../context/entries';
 
 export const NewEntry = () => {
+
+  const { addNewEntry } = useContext(EntriesContext)
+
+  const [isAdding, setIsAdding] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [touched, setTouched] = useState(false);
+
+  const onTextFieldChanged = (event:ChangeEvent<HTMLInputElement>) => {
+    setInputValue( event.target.value );
+  }
+
+  const onSave = () => {
+    if( inputValue.length === 0 ) return;
+    addNewEntry( inputValue );
+    setIsAdding( false );
+    setTouched( false );
+    setInputValue('');
+  }
+
   return (
-    <>
-      <Button>
-        
-      </Button>
-      <TextField 
-        fullWidth
-        sx={{ marginTop:2, marginBottom:1 }}
-        placeholder="Nueva entrada"
-        autoFocus
-        multiline
-        label="Nueva entrada"
-        helperText='Ingrese un valor'
-      />
-      <Box display='flex' justifyContent='space-between'>
-        <Button
-          variant='text'
-        >
-          Cancelar
-        </Button>
-        <Button
-          variant='outlined'
-          color='secondary'
-          endIcon={ <SaveOutlinedIcon /> }
-        >
-          Guardar
-        </Button>
-      </Box>
-      
-    </>
+    <Box sx={{ marginBottom:2, paddingX:2 }}>
+      {
+        isAdding 
+        ? (
+            <>
+              <TextField 
+                fullWidth
+                sx={{ marginTop:2, marginBottom:1 }}
+                placeholder="Nueva entrada"
+                autoFocus
+                multiline
+                label="Nueva entrada"
+                helperText={ inputValue.length <= 0 && touched && 'Ingrese un valor' }
+                error={ inputValue.length <= 0 && touched }
+                value={ inputValue }
+                onChange={ onTextFieldChanged }
+                onBlur={ () => setTouched(true) }
+              />
+              <Box display='flex' justifyContent='space-between'>
+                <Button
+                  variant='text'
+                  onClick={() => setIsAdding(false)}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  variant='outlined'
+                  color='secondary'
+                  endIcon={ <SaveOutlinedIcon /> }
+                  onClick={ onSave }
+                >
+                  Guardar
+                </Button>
+              </Box>
+            </>
+          )
+        : (
+            <Button
+              startIcon={ <AddCircleOutlineOutlinedIcon /> }
+              fullWidth
+              variant='outlined'
+              onClick={() => setIsAdding(true)}
+            >
+              Agregar Tarea
+            </Button>
+          )
+      }
+    </Box>
   )
 }
--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -35,9 +35,23 @@ export const EntriesProvider:FC<PropsWithChildren> = ({ children }) => {
 
   const [ state, dispatch ] = useReducer( entriesReducer, ENTRIES_INITIAL_STATE )
 
+  const addNewEntry = ( description:string ) => {
+
+    const newEntry:Entry = {
+      _id: uuidv4(),
+      description,
+      createdAt: Date.now(),
+      status: 'pending'
+    }
+
+    dispatch({ type:'[Entry] Add-Entry', payload:newEntry })
+
+  }
+
   return (
     <EntriesContext.Provider value={{
-      ...state
+      ...state,
+      addNewEntry
     }}>
       { children }
     </EntriesContext.Provider>
